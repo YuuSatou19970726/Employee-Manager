7b9c710ee12ed706c46d2d917ca0bf8d939fcecc
YuuSatou19970726 <44957064+YuuSatou19970726@users.noreply.github.com>
2019-10-30 19:50:23 +0700

    add new class (Employee(Adapter, List), ManagerEmployee(Add,Fragment, List), ErrorFragment) add new layout(employee_(adaper, list, manager_(fragment, list), error_fragment), update class MainActivity and layout employee_manager_add.

--- app/src/main/java/com/example/employeemanager/MainActivity.java
+++ app/src/main/java/com/example/employeemanager/MainActivity.java
@@ -23,21 +23,16 @@ public class MainActivity extends AppCompatActivity {
 
     public void onButtonLogin(View view) {
         // data send on Bundle (バンドルで送信（そうしん）されるデータ)
-        Bundle data = new Bundle();
-        String user = etUser.getText().toString();
-        String password = etPassword.getText().toString();
-        String manager = etManager.getText().toString();
-
-        data.putString("USER_ADMIN", user);
-        data.putString("PASSWORD_ADMIN", password);
-        data.putString("MANAGER_ADMIN", manager);
+//        Bundle data = new Bundle();
 
         // intent cau noi cac layout
-        Intent intent = new Intent(MainActivity.this, ManagerEmployee.class);
-        intent.putExtras(data);
+        Intent intent = new Intent(MainActivity.this, ManagerEmployeeFragment.class);
+//        intent.putExtras(data);
         startActivity(intent);
     }
 
     public void onButtonList(View view) {
+        Intent intent = new Intent(MainActivity.this, EmployeeList.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/employeemanager/ManagerEmployeeAdd.java
@@ -0,0 +1,39 @@
+package com.example.employeemanager;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class ManagerEmployeeAdd extends Fragment {
+
+    private TextView tvText;
+    private ButtonFragmentListener buttonFragmentListener;
+
+    @Nullable
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.employee_manager_add, container, false);
+
+        tvText = view.findViewById(R.id.tv_text);
+
+        return view;
+    }
+
+    public void setButtonFragmentListener(ButtonFragmentListener buttonFragmentListener) {
+        this.buttonFragmentListener = buttonFragmentListener;
+    }
+
+    public interface ButtonFragmentListener{
+        public void onAddSuccess();
+        public void onListSuccess();
+        public void onResetSuccess();
+    }
+}
